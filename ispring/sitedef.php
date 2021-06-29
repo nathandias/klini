f62f6598a939468855a9e49e2314b09f1cfd85d3
Nathan Dias <nathan.dias@gmail.com>
2021-06-29 14:06:25 -0700

    add example .htaccess file for dreamhost production systems, and add pagination variable setting

--- ispring/sitedef.php
+++ ispring/sitedef.php
@@ -2,17 +2,16 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-# instead of hardcoding username and password for the app in the source files
-# we read them from the evironment.
+# $development = false : read environment settings from the server environment
+# (which will be set in .htaccess for dreamhost shared hosting)
+# $development = true : reads environment settings through the vlucas/phpdotenv package
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
+$development = false;
 
-# The original ispring source code hard-coded some urls to point to
-# http://wordparts.net/ispring/*
-#
-# To facilitate migration and usage of the codebase on multiple domains
-# we extract out the base url and use that instead
+if ($development) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad();
+}
 
 $base_url = $_ENV['BASE_URL'];
 
@@ -27,7 +26,7 @@ $ispring_auth_pass = $_ENV['ISPRING_AUTH_PASS'];
 
 $ispring_department_id = $_ENV['ISPRING_DEPARTMENT_ID'];
 
-$pagination_limit = 10; # default
+$pagination_limit = $_ENV['PAGINATION_LIMIT']; # default
 if (isset($_GET['pagination_limit']) && is_numeric($_GET['pagination_limit']) && $_GET['pagination_limit'] > 0) {
     $pagination_limit = $_GET['pagination_limit'];
 }
